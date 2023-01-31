a5c8d128c4a63ebc98a810e54f44c7a2ddabbd3e
Marcelo Eugênio <123710944+CeloEug@users.noreply.github.com>
2023-01-31 20:27:58 -0300

    Update prettycalculator.py

--- prettycalculator.py
+++ prettycalculator.py
@@ -5,11 +5,10 @@ import mouse
 root = customtkinter.CTk()
 root.title("")
 root.resizable(False,False)
-root.attributes('-alpha',0.8) 
+root.attributes('-alpha',0.9) 
 root.overrideredirect(True)
 root.bind("<B1-Motion>", lambda e: event(e, Mode=True))
 root.bind("<ButtonRelease-1>", lambda e: standard_bind())
-root.geometry("200x400")
 
 global x, y
 
@@ -129,7 +128,7 @@ def event(widget, Mode=False):
     root.geometry('+%d+%d' % (mouse.get_position()[0]-x, mouse.get_position()[1]-y))
 
 ##numbers
-button0 = customtkinter.CTkButton(root, text="0", hover_color="#ffc041", fg_color="#ffa500",width=150, height=50, command=lambda:when_click(0))
+button0 = customtkinter.CTkButton(root, text="0", hover_color="#ffc041", fg_color="#ffa500",width=50, height=50, command=lambda:when_click(0))
 button1 = customtkinter.CTkButton(root, text="1", hover_color="#ffc041", fg_color="#ffa500",width=50, height=50, command=lambda:when_click(1))
 button2 = customtkinter.CTkButton(root, text="2", hover_color="#ffc041", fg_color="#ffa500",width=50, height=50,command=lambda:when_click(2))
 button3 = customtkinter.CTkButton(root, text="3", hover_color="#ffc041", fg_color="#ffa500",width=50, height=50,command=lambda:when_click(3))
@@ -144,47 +143,47 @@ button9 = customtkinter.CTkButton(root, text="9", hover_color="#ffc041", fg_colo
 buttonplus = customtkinter.CTkButton(root, width=50, height=50,text="+", fg_color="azure4", hover_color="azure3", command=button_add)
 buttonminus = customtkinter.CTkButton(root, width=50, height=50,text="-", fg_color="azure4", hover_color="azure3", command=button_sub)
 buttondiv = customtkinter.CTkButton(root, width=50, height=50,text="÷", fg_color="azure4", hover_color="azure3", command=button_div)
-buttonmul = customtkinter.CTkButton(root, width=50, height=50,text="x", fg_color="azure4", hover_color="azure3", command=button_mul)
+buttonmul = customtkinter.CTkButton(root, width=50, height=50,text="×", fg_color="azure4", hover_color="azure3", command=button_mul)
 buttonper = customtkinter.CTkButton(root, width=50, height=50,text="⅟", fg_color="azure4", hover_color="azure3", command=button_per)
 buttonexp = customtkinter.CTkButton(root, width=50, height=50,text="xˣ", fg_color="azure4", hover_color="azure3", command=button_exp)
 buttonrad = customtkinter.CTkButton(root, width=50, height=50,text="√ˣ", fg_color="azure4", hover_color="azure3", command=button_rad)
 
 ##functions
-buttonres = customtkinter.CTkButton(root, width=50, height=100,text="=", fg_color="#a22630", hover_color="#c63a4b", command=button_equal)
-buttonclear = customtkinter.CTkButton(root, width=100, height=50,text="C", fg_color="azure4", hover_color="azure3", command=button_clear)
+buttonres = customtkinter.CTkButton(root, width=50, height=50,text="=", fg_color="#a22630", hover_color="#c63a4b", command=button_equal)
+buttonclear = customtkinter.CTkButton(root, width=50, height=50,text="↺", fg_color="azure4", hover_color="azure3", command=button_clear)
 buttonback = customtkinter.CTkButton(root, width=50, height=50,text=".", fg_color="azure4", hover_color="azure3", command=lambda:when_click("."))
 buttonexit = customtkinter.CTkButton(root, corner_radius=0, width=40, height=25,text="✖", fg_color="#242424", hover_color="#a22630", command=stop_program)
 bardrag = customtkinter.CTkButton(root, corner_radius=0, width=25, height=25,text="☰", fg_color="#242424", hover_color="#242424")
 
 #grid assembling
-buttonclear.grid(row=7, column=0, rowspan=1, columnspan=2, sticky='ew')
-buttonback.grid(row=7, column=2, sticky='s')
-buttonmul.grid(row=7, column=3) 
 
-button0.grid(row=6, column=0, columnspan=3, sticky='w')
-buttondiv.grid(row=6, column=3)
+buttonmul.grid(row=6, column=3, padx=10, pady=10) 
+buttonclear.grid(row=6, column=0, sticky='w', columnspan=2, padx=10, pady=10)
+button0.grid(row=6, column=1, padx=10, pady=10)
+buttonback.grid(row=6, column=2, sticky='s', padx=10, pady=10)
 
-button1.grid(row=5, column=0, sticky='w')
-button2.grid(row=5, column=1)
-button3.grid(row=5, column=2)
-buttonplus.grid(row=5, column=3)
+button1.grid(row=5, column=0)
+button2.grid(row=5, column=1, padx=10, pady=10)
+button3.grid(row=5, column=2, padx=10, pady=10)
+buttondiv.grid(row=5, column=3, padx=10, pady=10)
 
-button4.grid(row=4, column=0, sticky='w')
-button5.grid(row=4, column=1)
-button6.grid(row=4, column=2)
-buttonminus.grid(row=4, column=3)
+button4.grid(row=4, column=0, sticky='w', padx=10, pady=10)
+button5.grid(row=4, column=1, padx=10, pady=10)
+button6.grid(row=4, column=2, padx=10, pady=10)
+buttonplus.grid(row=4, column=3, padx=10, pady=10)
 
-button7.grid(row=3, column=0, sticky='w')
-button8.grid(row=3, column=1)
-button9.grid(row=3, column=2)
+button7.grid(row=3, column=0, sticky='w', padx=10, pady=10)
+button8.grid(row=3, column=1, padx=10, pady=10)
+button9.grid(row=3, column=2, padx=10, pady=10)
+buttonminus.grid(row=3, column=3, padx=10, pady=10)
 
-buttonper.grid(row=2, column=0, sticky='nw')
-buttonexp.grid(row=2, column=1, sticky='n')
-buttonrad.grid(row=2, column=2, sticky='n')
-buttonres.grid(row=2, column=3, rowspan=2)
+buttonper.grid(row=2, column=0, sticky='nw', padx=10, pady=10)
+buttonexp.grid(row=2, column=1, sticky='n', padx=10, pady=10)
+buttonrad.grid(row=2, column=2, sticky='n', padx=10, pady=10)
+buttonres.grid(row=2, column=3, padx=10, pady=10)
 
-e.grid(row=1, column=0, columnspan=4, sticky='n')
-s.grid(row=1, column=0, columnspan=4, sticky='nw')
+e.grid(row=1, column=0, columnspan=4, sticky='ne', padx=10, pady=10)
+s.grid(row=1, column=0, columnspan=4, sticky='nw', padx=10, pady=10)
 
 buttonexit.grid(row=0, column=3, sticky='e')
 bardrag.grid(row=0, column=0, rowspan=4, sticky='nw')
